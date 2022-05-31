74664e867c5b901a6694fa358aa81b06e4df08ae
umakant-2113 <lodhiumakant800@gmail.com>
2022-05-31 14:44:09 +0530

    Done

---
+++ block-BNaacb/sample/modles/atricle.js
@@ -0,0 +1,9 @@
+let mongoose=require("mongoose");
+let schema=mongoose.schema;
+var userSchema= new schema({
+    author:"name of author",
+    discription:"about the article",
+title:"article title"
+
+
+})
