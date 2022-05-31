07d763ab5a62eab08d463d1cdf519557cde6ea70
umakant-2113 <lodhiumakant800@gmail.com>
2022-05-31 13:44:31 +0530

    Done

--- block-BNaabz/sample/index.js
+++ block-BNaabz/sample/index.js
@@ -1,10 +1,16 @@
 let express=require("express");
+
 const mongoose = require('mongoose');
+
 let app=express()
-mongoose.connect('mongodb://localhost/sample',(err)=>{
+
+mongoose.connect('mongodb://127.0.0.1/sample',(err)=>{
 console.log(err ? err :"mongoose are connected")
 });
 
+app.get("/",(req,res)=>{
+    res.send("welcome")
+})
 
 app.listen(3000,()=>{
     console.log("server on port 3k")
